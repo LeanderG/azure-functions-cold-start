2141400b13ed061b52b0e3a4014ee9ccb7af1c0d
Leander Gilles <leander.gilles@prodyna.com>
2021-06-24 23:16:42 +0200

    rename js to node

--- terraform/linux-node.tf
+++ terraform/linux-node.tf
@@ -1,5 +1,5 @@
-resource "azurerm_app_service_plan" "sp_coldstarts_linux_js" {
-  name                = "sp-coldstarts-linux-js"
+resource "azurerm_app_service_plan" "sp_coldstarts_linux_node" {
+  name                = "sp-coldstarts-linux-node"
   location            = azurerm_resource_group.rg_coldstarts.location
   resource_group_name = azurerm_resource_group.rg_coldstarts.name
   kind                = "Linux"
@@ -11,11 +11,11 @@ resource "azurerm_app_service_plan" "sp_coldstarts_linux_js" {
   }
 }
 
-resource "azurerm_function_app" "fa_coldstarts_linux-js" {
-  name                       = "fa-coldstarts-linux-js"
+resource "azurerm_function_app" "fa_coldstarts_linux-node" {
+  name                       = "fa-coldstarts-linux-node"
   location                   = azurerm_resource_group.rg_coldstarts.location
   resource_group_name        = azurerm_resource_group.rg_coldstarts.name
-  app_service_plan_id        = azurerm_app_service_plan.sp_coldstarts_linux_js.id
+  app_service_plan_id        = azurerm_app_service_plan.sp_coldstarts_linux_node.id
   storage_account_name       = azurerm_storage_account.sa_coldstarts.name
   storage_account_access_key = azurerm_storage_account.sa_coldstarts.primary_access_key
   os_type                    = "linux"
