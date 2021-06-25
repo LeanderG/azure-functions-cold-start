1f1ae5f132682d90b127a768aefc25c18646bca5
Leander Gilles <leander.gilles@prodyna.com>
2021-06-25 16:13:23 +0200

    remove some app service plans

--- terraform/linux-node.tf
+++ terraform/linux-node.tf
@@ -1,11 +1,10 @@
-// small node function
-resource "azurerm_app_service_plan" "sp_coldstarts_linux_node_sm" {
-  name                = "sp-coldstarts-linux-node-sm"
+
+resource "azurerm_app_service_plan" "sp_coldstarts_linux_node" {
+  name                = "sp-coldstarts-linux-node"
   location            = azurerm_resource_group.rg_coldstarts.location
   resource_group_name = azurerm_resource_group.rg_coldstarts.name
   kind                = "Linux"
   reserved            = true
-
   sku {
     tier = "Dynamic"
     size = "Y1"
@@ -16,57 +15,27 @@ resource "azurerm_function_app" "fa_coldstarts_linux_node_sm" {
   name                       = "fa-coldstarts-linux-node-sm"
   location                   = azurerm_resource_group.rg_coldstarts.location
   resource_group_name        = azurerm_resource_group.rg_coldstarts.name
-  app_service_plan_id        = azurerm_app_service_plan.sp_coldstarts_linux_node_sm.id
+  app_service_plan_id        = azurerm_app_service_plan.sp_coldstarts_linux_node.id
   storage_account_name       = azurerm_storage_account.sa_coldstarts.name
   storage_account_access_key = azurerm_storage_account.sa_coldstarts.primary_access_key
   os_type                    = "linux"
-}
-
-
-// medium node function
-resource "azurerm_app_service_plan" "sp_coldstarts_linux_node_md" {
-  name                = "sp-coldstarts-linux-node-md"
-  location            = azurerm_resource_group.rg_coldstarts.location
-  resource_group_name = azurerm_resource_group.rg_coldstarts.name
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
 }
 
 resource "azurerm_function_app" "fa_coldstarts_linux_node_md" {
   name                       = "fa-coldstarts-linux-node-md"
   location                   = azurerm_resource_group.rg_coldstarts.location
   resource_group_name        = azurerm_resource_group.rg_coldstarts.name
-  app_service_plan_id        = azurerm_app_service_plan.sp_coldstarts_linux_node_md.id
+  app_service_plan_id        = azurerm_app_service_plan.sp_coldstarts_linux_node.id
   storage_account_name       = azurerm_storage_account.sa_coldstarts.name
   storage_account_access_key = azurerm_storage_account.sa_coldstarts.primary_access_key
   os_type                    = "linux"
-}
-
-
-// large node function
-resource "azurerm_app_service_plan" "sp_coldstarts_linux_node_lg" {
-  name                = "sp-coldstarts-linux-node-lg"
-  location            = azurerm_resource_group.rg_coldstarts.location
-  resource_group_name = azurerm_resource_group.rg_coldstarts.name
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
 }
 
 resource "azurerm_function_app" "fa_coldstarts_linux_node_lg" {
   name                       = "fa-coldstarts-linux-node-lg"
   location                   = azurerm_resource_group.rg_coldstarts.location
   resource_group_name        = azurerm_resource_group.rg_coldstarts.name
-  app_service_plan_id        = azurerm_app_service_plan.sp_coldstarts_linux_node_lg.id
+  app_service_plan_id        = azurerm_app_service_plan.sp_coldstarts_linux_node.id
   storage_account_name       = azurerm_storage_account.sa_coldstarts.name
   storage_account_access_key = azurerm_storage_account.sa_coldstarts.primary_access_key
   os_type                    = "linux"
