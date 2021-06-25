d5cdfc600d978dafbfbc072c813f5f05c5c999dc
Leander Gilles <leander.gilles@prodyna.com>
2021-06-25 19:20:23 +0200

    add md sized function to action

--- functions/linux-node-md/ColdstartsLinuxNodeMdHttpTrigger/index.js
+++ functions/linux-node-md/ColdstartsLinuxNodeMdHttpTrigger/index.js
@@ -1,9 +1,9 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('JavaScript HTTP trigger function processed a request. Medium sized function');
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully. Medium sized function"
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
